7ff276b29b363008b9ba54965aa48eb98b995141
santiago-rodrig <santo1996.29@gmail.com>
2021-02-22 20:50:22 -0400

    complete the implementation of SecondHand

--- main_test.go
+++ main_test.go
@@ -16,15 +16,15 @@ func TestSecondHandAtMidnight(t *testing.T) {
 	}
 }
 
-//func TestSecondHand(t *testing.T) {
-//	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
-//	want := Point{X: 150, Y: 150 + 90}
-//	got := SecondHand(tm)
-//
-//	if got != want {
-//		t.Errorf("got %v, wanted %v", got, want)
-//	}
-//}
+func TestSecondHand(t *testing.T) {
+	tm := time.Date(1337, time.January, 1, 0, 0, 30, 0, time.UTC)
+	want := Point{X: 150, Y: 150 + 90}
+	got := SecondHand(tm)
+
+	if got != want {
+		t.Errorf("got %v, wanted %v", got, want)
+	}
+}
 
 func TestSecondsInRadians(t *testing.T) {
 	cases := []struct {
